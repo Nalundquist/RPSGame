4b22f16ae61b32367ddb90287e163777686e4841
Noah Lundquist <nalundquist@gmail.com>
2022-09-23 10:45:35 -0700

    add dispose; fulfill Bread test 3; add fourth

--- Pierre/Bakery.Tests/ModelTests/Bread.Tests.cs
+++ Pierre/Bakery.Tests/ModelTests/Bread.Tests.cs
@@ -1,12 +1,18 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Bake.Loaves;
+// using System.Collections.Generic;
+// using Bake.Loaves;
 using System;
 
 namespace Bake.Loaves.Tests
 {
 	[TestClass]
-	public class BreadTests
+	public class BreadTests : IDisposable
 	{
+		public void Dispose()
+		{
+			Bread.ClearOrder();
+		}
+
 		[TestMethod]
 		public void Bread_HoldsLoaf_True()
 		{
@@ -16,15 +22,26 @@ namespace Bake.Loaves.Tests
 		[TestMethod]
 		public void Bread_HoldsPrice_True()
 		{
-			Bread newPump = new Bread("Pumpernickel", 2);
-			Assert.AreEqual(2, newPump.BreadPrice);
+			Bread newPump = new Bread("Pumpernickel", 5);
+			Assert.AreEqual(5, newPump.BreadPrice);
 		}
 		[TestMethod]
 		public void BreadTab_ReturnsBreadPrices_True()
 		{
-			Bread newPump = new Bread("Pumpernickel", 2);
-			Bread newOlive = new Bread("Olive", 2);
-			Assert.AreEqual(4, Bread.BreadTab());
+			Bread newPump = new Bread("Pumpernickel", 5);
+			Bread newOlive = new Bread("Olive", 5);
+			Assert.AreEqual(10, Bread.BreadTab());
+		}
+		[TestMethod]
+		public void BreadTab_ReturnsDiscountPrice_True()
+		{
+			Bread newPump = new Bread("Pumpernickel", 5);
+			Bread newOlive = new Bread("Olive", 5);
+			Bread newPump2 = new Bread("Pumpernickel", 5);
+			Bread newOlive2 = new Bread("Olive", 5);
+			Bread newPump3 = new Bread("Pumpernickel", 5);
+			Bread newOlive3 = new Bread("Olive", 5);
+			Assert.AreEqual(20, Bread.BreadTab());
 		}
 	}
 }
--- Pierre/Bakery/Models/Bread.cs
+++ Pierre/Bakery/Models/Bread.cs
@@ -7,14 +7,27 @@ namespace Bake.Loaves
 	{
 		public string BreadType { get; set; }
 		public int BreadPrice { get; set; }
+		private static List<Bread> _breadOrder = new List<Bread> {};
 		public Bread(string breadType, int breadPrice)
 		{
 			BreadType = breadType;
 			BreadPrice = breadPrice;
+			_breadOrder.Add(this);
 		}
 		public static int BreadTab()
 		{
-			return 0;
+			int orderPrice = 0;
+			foreach (Bread bread in _breadOrder)
+			{
+			orderPrice += bread.BreadPrice;
+			Console.WriteLine("Bread Price = " + bread.BreadPrice);
+			Console.WriteLine("Order Price = " + orderPrice);
+			}
+			return orderPrice;
+		}
+		public static void ClearOrder()
+		{
+			_breadOrder.Clear();
 		}
 	}
 }
